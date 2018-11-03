cd6d3bff6242864a290a986c62fd87a12faaad46
xxh <st75033562@163.com>
2018-11-03 17:24:04 +0800

    init

---
+++ Assets/GameController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour {
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private GameObject bip;
+    [SerializeField]
+    private Shader shader;
+	// Use this for initialization
+	void Start () {
+        List<CombineInstance> combineInstances = new List<CombineInstance> ();
+        List<Material> materials = new List<Material> ();
+        List<Transform> bones = new List<Transform> ();
+
+        SkinnedMeshRenderer[] skinnedMeshs = player.GetComponentsInChildren<SkinnedMeshRenderer> ();
+
+        Transform[] transforms = player.GetComponentsInChildren<Transform> ();
+        foreach(SkinnedMeshRenderer smr in skinnedMeshs) {
+            materials.AddRange (smr.sharedMaterials);
+            for(int sub = 0; sub < smr.sharedMesh.subMeshCount; sub++) {
+                CombineInstance ci = new CombineInstance ();
+                ci.mesh = smr.sharedMesh;
+                ci.subMeshIndex = sub;
+                combineInstances.Add (ci);
+            }
+            int startBoneIndex = bones.Count;
+            foreach(Transform t in smr.bones) {
+                foreach(Transform transform in transforms) {
+                    if(transform.name != t.name)
+                        continue;
+                    bones.Add (transform);
+                    break;
+                }
+            }
+
+            int endBoneIndex = bones.Count;
+            for(int i = 1; i < smr.sharedMesh.subMeshCount; ++i) {
+                for(int j = startBoneIndex; j < endBoneIndex; ++j) {
+                    bones.Add (bones[j]);
+                }
+            }
+        }
+
+        GameObject go  = Instantiate (bip);
+        SkinnedMeshRenderer r = go.AddComponent<SkinnedMeshRenderer> ();
+        r.sharedMesh = new Mesh ();
+        r.sharedMesh.CombineMeshes (combineInstances.ToArray () , false , false);
+        r.bones = bones.ToArray ();
+        r.materials = materials.ToArray ();
+
+        player.SetActive (false);
+
+        foreach (Material material in go.GetComponent<Renderer>().materials) {
+            material.shader = shader;
+            material.SetFloat ("_HeightVal" , 0);
+            material.SetFloat ("_TransparentLen" , 0.5f);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/shader/TransparentPart.shader
@@ -0,0 +1,44 @@
+﻿Shader "Custom/Transparent/part" {
+	Properties{
+		_MainTex("Base (RGB)", 2D) = "white" {}
+		_HeightVal("height Value", Float) = 1
+		_TransparentLen("transparent length", Range(0 , 100)) = 0.7 //控制在最后距离中不再计算渐变直接透明
+		_TransitionVal("transition Value", Range(1, 100)) = 1.2
+	}
+	SubShader{
+		Tags{ "RenderType" = "Opaque" "Queue" = "Transparent" }
+		LOD 200
+
+		Pass{ ColorMask 0 }  //只渲染深度
+
+		CGPROGRAM
+		#pragma surface surf Lambert alpha vertex:Myvert  
+		sampler2D _MainTex;
+		float _HeightVal;
+		float _TransparentLen;
+		float _TransitionVal;
+
+		struct Input {
+			float2 uv_MainTex;
+			float3 vertex;
+		};
+
+		void Myvert(inout appdata_full v, out Input IN){
+			UNITY_INITIALIZE_OUTPUT(Input, IN);
+			IN.vertex = v.vertex.xyz;  //本地坐标
+		}
+
+		void surf(Input IN, inout SurfaceOutput o) {
+			half4 c = tex2D(_MainTex, IN.uv_MainTex);
+			o.Albedo = c.rgb;
+			
+			if (IN.vertex.z > _HeightVal) {
+				o.Alpha = 1;
+			}else {
+				o.Alpha = (1 - abs(IN.vertex.z) / _TransitionVal) - _TransparentLen;
+			}	
+		}
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
